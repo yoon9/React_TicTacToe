27fbddd41566e8e765b307fe04a45374d983ca15
yunsu <yunsu1502@naver.com>
2024-04-08 01:50:44 +0900

    feat: square 클릭 시 X 표시

---
+++ src/components/Board.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Square from '../components/Square';
+import './Board.css';
+
+
+export default class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares})
+  }
+  
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={()=> this.handleClick(i)}/>
+  }
+
+  render() {
+    const status = 'Next player: X,O';
+    
+    return (
+      <div>
+        <div className='status'>{status}</div>
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
